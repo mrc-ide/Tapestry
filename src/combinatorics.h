b3d8ab0677d74cb9d8eb3ec734bc42ab756d508b
JasonAHendry <jason.ahendry@gmail.com>
2022-10-27 14:14:55 +0100

    Functions for computing the powerset and all possible partitions.

---
+++ src/combinatorics.h
@@ -0,0 +1,7 @@
+#include <vector>
+#include <cmath>
+using namespace std;
+
+vector<vector<int>> create_powerset(int K);
+
+vector<vector<vector<int>>> create_all_partitions(vector<int> collection);
