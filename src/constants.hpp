d5c752c34f47af9940970d356ffefa5dedac9dd9
JasonAHendry <jason.ahendry@gmail.com>
2023-03-13 06:09:16 +0000

    Bell number constants should not be extern, but const.

--- src/constants.hpp
+++ src/constants.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
-extern int BELL_NUMBERS[11];
+// Bell numbers one to ten; from https://oeis.org/A000110
+const int BELL_NUMBERS[11] = {
+        1,
+        1,
+        2,
+        5,
+        15,
+        52,
+        203,
+        877,
+        4140,
+        21147,
+        115975
+};
