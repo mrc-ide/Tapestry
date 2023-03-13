1326c3c3c857e1af41f2fb2df35910f1398e26a6
JasonAHendry <jason.ahendry@gmail.com>
2023-03-13 06:14:18 +0000

    Switch to Eigen array typedefs.

--- src/typedefs.hpp
+++ src/typedefs.hpp
@@ -1,6 +1,8 @@
 #pragma once
-#include "boost/multi_array.hpp"
+#include "libs/eigen-3.4.0/Dense"
 
-using matrix_2d_int     = boost::multi_array<int, 2>;
-using matrix_2d_double  = boost::multi_array<double, 2>;
-using matrix_3d_double  = boost::multi_array<double, 3>;
+
+using Eigen::MatrixXd;  // matrix of doubles
+using Eigen::MatrixXi;  // matrix of ints
+using Eigen::ArrayXd;   // array of doubles
+
