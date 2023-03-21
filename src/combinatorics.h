b1056409be3256f9f8c26db160ee30c9b6df6a1c
JasonAHendry <jason.ahendry@gmail.com>
2023-03-21 14:41:14 +0100

    Remove old source files.

--- src/combinatorics.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <vector>
-#include "typedefs.hpp"
-using namespace std;
-
-matrix_2d_int create_powerset(int K);
-vector<vector<vector<int>>> create_all_partitions(vector<int> collection);
--- src/typedefs.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "libs/eigen-3.4.0/Dense"
-
-
-using Eigen::MatrixXd;  // matrix of doubles
-using Eigen::MatrixXi;  // matrix of ints
-using Eigen::ArrayXd;   // array of doubles
-
