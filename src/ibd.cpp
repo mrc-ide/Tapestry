0dbbbb4d67385e4c765a41c726436335b5419858
JasonAHendry <jason.ahendry@gmail.com>
2023-04-04 08:19:34 +0200

    Improve commenting, add '.pair_names' member.

--- src/ibd.cpp
+++ src/ibd.cpp
@@ -1,6 +1,9 @@
-#include <utility>
 #include <numeric>
+#include <sstream>
+#include <string>
+#include <utility>
 #include <vector>
+#include "bed.hpp"
 #include "combinatorics.hpp"
 #include "ibd.hpp"
 #include "libs/eigen-3.4.0/Dense"
@@ -8,10 +11,17 @@ using Eigen::MatrixXi;
 using namespace std;
 
 
+// ================================================================================
+// Container for IBD information
+//
+// ================================================================================
+
+
 IBDContainer::IBDContainer(int K)
     : strains(create_strains(K)),
     states(create_all_partitions(strains)),
     column_index_to_pair(get_unique_pairs(strains)),
+    pair_names(create_pair_names(column_index_to_pair)),
     pair_to_column_index(create_pair_to_column_index(strains)),
     state_index_to_column_indices(create_state_index_to_column_indices(states, pair_to_column_index))
 {}
@@ -22,6 +32,18 @@ vector<int> IBDContainer::create_strains(int K)
     vector<int> strains(K);
     iota(strains.begin(), strains.end(), 0);
     return strains;
+}
+
+vector<string>  IBDContainer::create_pair_names(const vector<pair<int, int>>& pairs)
+{
+    //string pair_name;
+    vector<string> pair_names;
+    for (const pair<int, int>& pair : pairs) {
+        ostringstream pair_name;
+        pair_name << "strains" << pair.first << "-" << pair.second;
+        pair_names.push_back(pair_name.str());
+    }
+    return pair_names;
 }
 
 
@@ -71,4 +93,74 @@ vector<vector<int>> IBDContainer::create_state_index_to_column_indices(
     return mapping;
 }
 
+
+
+// ================================================================================
+// IBD Segment Detection
+//
+// ================================================================================
+
+
+
+// TODO:
+// - What is the most efficient way to populate the vector of records?
+// - What about copying it out of the scope?
+std::vector<BEDRecord> get_ibd_segments(
+    std::vector<std::string> chroms,
+    VectorXi pos,
+    VectorXi ibd_states, // TODO: do I need this to be explicitly bool?
+    std::string name
+)
+{
+    // Init
+    int t = 0;
+    int start, end;
+    std::string chrom = chroms[t];
+    int ibd_state = ibd_states(t);
+    if (ibd_state) {
+        start = pos(t);
+    }
+
+    // Iterate
+    //BEDRecord record;
+    vector<BEDRecord> bed_records;  // we don't know the size a priori
+    for (; t < pos.size(); ++t) {
+        
+        // Handle end of chromosome
+        if (chroms[t] != chrom) {
+            if (ibd_state) {
+                end = pos(t-1);
+                bed_records.push_back(BEDRecord(chrom, start, end, name));
+            }
+            if (ibd_states(t-1)) {
+                start = pos(t);
+            }
+            chrom = chroms[t];
+            ibd_state=ibd_states(t);
+            continue;
+        }
+
+        // Handle chromosome internal
+        if (ibd_state) {
+            if (!ibd_states(t)) {  // segment ending
+                end = (pos(t-1) + pos(t)) / 2;
+                bed_records.push_back(BEDRecord(chrom, start, end, name));
+            } 
+        } else if (ibd_states(t)) { // segment starting
+            start = (pos(t-1) + pos(t)) / 2;
+        }
+
+        // Update memory
+        chrom = chroms[t];
+        ibd_state = ibd_states(t);
+    }
+
+    // Terminate
+    if (ibd_state) {
+        end = pos(t);
+        bed_records.push_back(BEDRecord(chrom, start, end, name));
+    }
+
+    return bed_records;
+}
 
--- src/ibd.hpp
+++ src/ibd.hpp
@@ -1,15 +1,25 @@
 #pragma once
+#include <string>
 #include <utility>
 #include <vector>
+#include "bed.hpp"
 #include "libs/eigen-3.4.0/Dense"
 using Eigen::MatrixXi;
+using Eigen::VectorXi;
 using namespace std;
+
+
+// ================================================================================
+// Container for IBD information
+//
+// ================================================================================
 
 
 class IBDContainer
 {
 private:
     vector<int> static create_strains(int K);
+    vector<string> static create_pair_names(const vector<pair<int, int>>& pairs);
     MatrixXi static create_pair_to_column_index(const vector<int>& strains);
     vector<vector<int>> static create_state_index_to_column_indices(
         const vector<vector<vector<int>>>& states,
@@ -19,29 +29,59 @@ private:
 public:
     IBDContainer(int K);
 
-    // Integer vector of strains, e.g. if K=3, {0, 1, 2}
+    /* Strain indices
+    *  e.g., if K=3, {0, 1, 2}
+    */
     const vector<int> strains;
 
-    // All possible IBD states
-    // size = Bell(k)
-    // correspond to hidden states in HMM
-    // first index is *state*
-    // second inndex is *strain group*; 
-    // e.g. if K=3 ...
-    // {{{0},{1},{2}}, {{0,1}, {2}}, {{0,2},{1}}, {{0},{1,2}}, {{0,1,2}}
+    /* All possible IBD states
+    *  Here, by IBD state, we mean a particular *hidden state* in the
+    *  HMM that defines our model. For higher K, these IBD states 
+    *  define *pairs* or *groups* of strains that are in IBD.
+    *  There are Bell(K) number of IBD states.
+    *  e.g. if K=3...
+    *
+    *  {{{0},{1},{2}}, {{0,1}, {2}}, {{0,2},{1}}, {{0},{1,2}}, {{0,1,2}}
+    *
+    */
     const vector<vector<vector<int>>> states;
 
-    // Return the pair given a column index
+    /* For each column in a pairwise IBD matrix, which *pair* of strains
+    *  is in IBD?
+    */
     const vector<pair<int, int>> column_index_to_pair;
 
-    // We also have a notion of (K choose 2) IBD pairs
-    // these pairs have a (semi) arbitrary order (the 'column index')
-    // so here... pair_to_column_index(0, 1) = 2
-    // ... would mean {0, 1} pair has column index 2
+    /* Names for pairs of strains in IBD
+    *  e.g., strains0-1
+    */
+    const vector<string> pair_names;
+
+
+    /* Inverse of `column_index_to_pair`
+    *  We have  a notion  of (K choose 2) IBD pairs. We give these pairs
+    *  a (semi-arbitary) order, defined in `column_index_to_pair`.
+    *  Here, pair_to_column_index(0, 1) = 2 would mean IBD state information
+    *  about strains {0, 1} is stored in column 2 of the pairwise matrix.
+    */
     const MatrixXi pair_to_column_index;
 
-    // Go from IBD state index to all implied pairwise column indices
-    // Note this will be ragged
+    /* Map from IBD state (i.e. from HMM) to column indices of
+    *  all pairs of strains that are in IBD.
+    *
+    */
     const vector<vector<int>> state_index_to_column_indices;
 };
 
+
+// ================================================================================
+// IBD Segment Detection
+//
+// ================================================================================
+
+
+std::vector<BEDRecord> get_ibd_segments(
+    std::vector<std::string> chroms,
+    VectorXi pos,
+    VectorXi ibd_states, // TODO: do I need this to be explicitly bool?
+    std::string name
+);
